b29793ac17dc10a307738b95b07c1fc299c8a234
Prakhar <prakharyadav27@gmail.com>
2020-09-02 20:05:38 +0530

    cleaned code

--- helloworld.py
+++ helloworld.py
@@ -1,15 +1,13 @@
 print('hello world')
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
 for i in range(10):
 	print('Hi there')
-=======
-print('made some more changes here')
->>>>>>> 1da5329ba4dba8ef5c44f3e536f7a62f956da98d
-=======
-
 
 print('made some more changes here')
 
->>>>>>> 2aceae09be7090915e2a5f5b9bde6cba84f84501
+
+
+print('made some more changes here')
+
+
